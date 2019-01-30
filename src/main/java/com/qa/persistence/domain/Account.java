162eeeec811dba14d6b37dcc63ca599c922bdb06
Admin <Admin@STUDENT20>
2019-01-30 16:08:02 +0000

    add entity. altered pom for h2

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,13 +1,25 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 	
-	String firstName;
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long Id;
 	
-	String lastName;
-	
-	Long accountNumber;
+	private Long accountNumber;
+	private String firstName;
+	private String lastName;
 
+	public Account(String firstName, String lastName) {
+		this.firstName =  firstName;
+		this.lastName = lastName;
+	}
+	
 	public Account(String firstName, String lastName, Long accountNumber) {
 		this.firstName =  firstName;
 		this.lastName = lastName;
